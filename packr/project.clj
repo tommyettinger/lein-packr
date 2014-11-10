c927432c3fb8437026b99080d01a798a3a3756bc
Tommy Ettinger <tommy.ettinger@gmail.com>
2014-11-10 05:17:45 -0800

    Not complete. Can download JREs, run Packr.

---
+++ packr/project.clj
@@ -0,0 +1,7 @@
+(defproject clj.lein/packr "0.1.0-SNAPSHOT"
+  :description "Packs an uberjar with a bundled JRE"
+  :url "https://github.com/tommyettinger/lein-packr"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[com.badlogicgames.packr/packr "1.2"]]
+  :eval-in-leiningen true)
